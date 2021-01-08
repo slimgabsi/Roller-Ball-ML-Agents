d76b80a9439b8719d5963af5bbe116be8651a511
Slim Gabsi <Slimgabsi287@gmail.com>
2021-01-08 20:46:15 +0100

    commit_02

---
+++ UnityProject/Assets/RollerBall/Scripts/RollerBallAgent.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class RollerBallAgent : Agent
+{
+    Rigidbody rBody;
+    public float forceMultiplier = 10;
+    public override void Initialize()
+    {
+        rBody = GetComponent<Rigidbody>();
+
+    }
+    public Transform Target;
+    public override void OnEpisodeBegin()
+    {
+
+        // If the Agent fell, zero its momentum
+        if (transform.localPosition.y < 0)
+        {
+            rBody.angularVelocity = Vector3.zero;
+            rBody.velocity = Vector3.zero;
+            transform.localPosition = new Vector3(0, 0.5f, 0);
+        }
+
+        // Move the target to a new spot
+        Target.localPosition = new Vector3(Random.value * 8 - 4,
+                                           0.5f,
+                                           Random.value * 8 - 4);
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+
+        // Target and Agent positions
+        sensor.AddObservation(Target.localPosition);
+        sensor.AddObservation(transform.localPosition);
+
+        // Agent velocity
+        sensor.AddObservation(rBody.velocity.x);
+        sensor.AddObservation(rBody.velocity.z);
+    }
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = vectorAction[0];
+        controlSignal.z = vectorAction[1];
+        rBody.AddForce(controlSignal * forceMultiplier);
+
+        // Rewards
+        float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
+
+        // Reached target
+        if (distanceToTarget < 1.42f)
+        {
+            SetReward(1.0f);
+            EndEpisode();
+        }
+
+        // Fell off platform
+        else if (this.transform.localPosition.y < 0)
+        {
+            EndEpisode();
+        }
+    }
+    public override void Heuristic(float[] actionsOut)
+    {
+
+        actionsOut[0] = Input.GetAxis("Horizontal");
+        actionsOut[1] = Input.GetAxis("Vertical");
+    }
+}
